9c2decee3f05f31486b03c816ad1507b74258c13
Rohit Kumar <75325602+Rohit-Alex@users.noreply.github.com>
2026-02-15 19:53:52 +0530

    Update index.ts

--- src/pages/Dashboard/index.ts
+++ src/pages/Dashboard/index.ts
@@ -1,4 +1,3 @@
-export { default as Dashboard } from './Dashboard'
 export { default as DeviceControlDashboard } from './DeviceControl'
 export { default as DataTransferDashboard } from './DataTransfer'
 export { default as ApplicationControlDashboard } from './ApplicationControl'
